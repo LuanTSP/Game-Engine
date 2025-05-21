9b8791bc474b5b84868a1b1d73d07ba47c9b5359
LuanTSP <luanpacifico@gmail.com>
2025-05-20 23:51:13 -0300

    added cmake build system

---
+++ demo/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/link.d"
+  "CMakeFiles/demo.dir/main.cpp.o"
+  "CMakeFiles/demo.dir/main.cpp.o.d"
+  "CMakeFiles/demo.dir/src/scenes/scene1.cpp.o"
+  "CMakeFiles/demo.dir/src/scenes/scene1.cpp.o.d"
+  "CMakeFiles/demo.dir/src/scenes/scene2.cpp.o"
+  "CMakeFiles/demo.dir/src/scenes/scene2.cpp.o.d"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
